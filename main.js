7fd950a1b842f60710de78bac96bcfb6358a31e1
hebrahimachuki <achukiibrahim@yahoo.com>
2021-09-08 18:22:47 +0300

    add days of week variables and gender names

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ var dateOfBirth=parseInt(document.getElementById("date").value);
 var months=parseInt(document.getElementById("month").value);
 var years=parseInt(document.getElementById("year").value);
 var genders=(document.getElementById("gender").checked);
-var genderses=(document.getElementById("gender1").checked)
-var daysOfweek["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"]
+var genderses=(document.getElementById("gender1").checked);
+var daysOfWeek=["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday", "Friday", "Saturday"];
+var femaleName=["Akoko","Adwoa","Abena","Akua","Yaa","Afua","Ama"];
+var maleName=["Kwasi","Kwadwo","Kwabera","Kwaku","Yaw","Kofi","Kwame"];
+var day=
 
+    
+    if(document.getElementById("gender").checked){
+        document.getElementById("display").innerHTML=
+        document.getElementById("gender").value;
+
+    }else{
+        document.getElementById("display").innerHTML=
+        "no one select";var
+    }    
 
 
 
@@ -14,7 +26,5 @@ var daysOfweek["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Satu
 
 
 }
-
-
 
 
