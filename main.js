9c6559852b7423d121405f6f41acebeeff6f038d
hebrahimachuki <achukiibrahim@yahoo.com>
2021-09-08 13:48:52 +0300

    add javascript variables

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 function result() {
 
-var dateOfBirth=parseInt(document.getElementById("date"));
-var months=(document.getElementById("month"));
-var years=(document.getElementById("year"));
-var genders=(document.getElementById("gender"));
+var dateOfBirth=parseInt(document.getElementById("date").value);
+var months=parseInt(document.getElementById("month").value);
+var years=parseInt(document.getElementById("year").value);
+var genders=(document.getElementById("gender").checked);
+var genderses=(document.getElementById("gender1").checked)
+var daysOfweek["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"]
 
-var result=parseInt(dateOfBirth);
-document.getElementById("display").innerHTML= "Im trying result function" + result;
 
-//console.log(result);
+
+
+
+
+
 }
+
+
+
+
